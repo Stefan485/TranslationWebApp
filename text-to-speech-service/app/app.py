b7ccef4a56b7f40b6f71a7b5b6153abb9b3e231b
Stefan485 <obradovic.stefan485@gmail.com>
2026-08-09 18:30:54 +0200

    init app

---
+++ text-to-speech-service/app/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from model import tts_model
+
+
+app = FastAPI()
+
+
+class TTSRequest(BaseModel):
+    text: str
+    language: str = "en"
+
+
+
+@app.post("/tts")
+def text_to_speech(request: TTSRequest):
+
+    file = tts_model.generate_speech(
+        request.text,
+        request.language
+    )
+
+    return {
+        "audio_file": file
+    }
